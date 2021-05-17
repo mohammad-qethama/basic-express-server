167a6482a58b56f3241febda15bec2ce0c7b7ba1
mohammad-qethama <mhmd.a@protonmail.com>
2021-05-17 18:36:24 +0300

    done tests

---
+++ __tests__/logger.test.js
@@ -0,0 +1,25 @@
+'use strict';
+const logger = require('../src/middleware/logger.js');
+
+describe('logger',()=>{
+   let consoleSpy;
+   const req = {method:'get', path:'test' };
+   const res = {};
+   const next = jest.fn(); 
+
+   beforeEach(()=>{
+       consoleSpy = jest.spyOn(console,'log');
+
+   });
+   afterEach(() => {
+    consoleSpy.mockRestore();
+});
+
+it('should log all get requests', () => {
+
+    logger(req, res, next);
+    expect(consoleSpy).toHaveBeenCalledWith( 'get', 'test')
+    expect(next).toHaveBeenCalledWith();
+});
+ 
+})
